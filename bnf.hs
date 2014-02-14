34b82bb69a32509c63821f1722b6686936684078
joneshf <jones3.hardy@gmail.com>
2014-02-14 14:59:23 -0800

    Trying to get more than one production at a time.

--- bnf.hs
+++ bnf.hs
@@ -5,6 +5,7 @@ import Control.Applicative ((<*>), (<$>), (<*))
 import Data.Generics
 import Data.List.NonEmpty
 import Text.Parsec
+--import Text.Parsec.Extra
 
 data BNF = BNF (NonEmpty BNFProduction)
     deriving (Eq, Show, Typeable, Data)
@@ -20,7 +21,7 @@ data BNFBase = NonTerminal String
     deriving (Eq, Show, Typeable, Data)
 
 bnf :: Stream s m Char => ParsecT s u m BNF
-bnf = BNF <$> parsecMap fromList (spaces >> production `sepBy1` spaces)
+bnf = BNF <$> parsecMap fromList (spaces >> production `sepBy1` newline)
 
 production :: Stream s m Char => ParsecT s u m BNFProduction
 production = BNFProduction <$> anyBetweenAngle <*
@@ -30,13 +31,18 @@ production = BNFProduction <$> anyBetweenAngle <*
 
 bnfExpr :: Stream s m Char => ParsecT s u m BNFExpr
 bnfExpr = BNFExpr <$> parsecMap fromList
-                      ((nonTerminal <|> terminal) `sepEndBy1` spaces)
+                      (manyTill base (lookAhead (try (base >> string "::="))))
+
+base :: Stream s m Char => ParsecT s u m BNFBase
+base = (nonTerminal <|> terminal) <* spaces
 
 nonTerminal :: Stream s m Char => ParsecT s u m BNFBase
 nonTerminal = NonTerminal <$> anyBetweenAngle
 
 terminal :: Stream s m Char => ParsecT s u m BNFBase
 terminal = Terminal <$> anyBetweenDoubleQ
+
+-- Helpers
 
 anyBetweenAngle :: Stream s m Char => ParsecT s u m String
 anyBetweenAngle = anyBetween (char '<') (char '>')
@@ -50,6 +56,8 @@ anyBetween :: Stream s m Char
            -> ParsecT s u m String
 anyBetween open close = open >> manyTill anyChar (try close)
 
+-- Examples
+
 postalAddress :: String
 postalAddress = Prelude.unlines
     [ "<postal-address> ::= <name-part> <street-address> <zip-part>"
@@ -60,4 +68,10 @@ postalAddress = Prelude.unlines
     , "<zip-part> ::= <town-name> \",\" <state-code> <ZIP-code> <EOL>"
     , "<opt-suffix-part> ::= \"Sr.\" | \"Jr.\" | <roman-numeral> | \"\""
     , "<opt-apt-num> ::= <apt-num> | \"\""
+    ]
+
+namePart :: String
+namePart = Prelude.unlines
+    [ "<name-part> ::= <personal-part> <last-name> <opt-suffix-part> <EOL>"
+    , "            | <personal-part> <name-part>"
     ]
